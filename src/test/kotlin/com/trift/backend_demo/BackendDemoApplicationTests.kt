12a57016f4ff716d146a49335e91ed1b4a371d4d
2480258 <swh8059@naver.com>
2023-06-30 04:28:22 +0900

    Initial Commit

---
+++ src/test/kotlin/com/trift/backend_demo/BackendDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.trift.backend_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
